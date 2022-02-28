e6037d1beb00839d12f6b5284721d5bc222cd3bf
Wojtysek <vojtahr1@post.cz>
2022-02-28 12:59:32 +0100

    Přidejte soubory projektu.

---
+++ cifernysoucet/cifernysoucet.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int cifra;
+	int soucet = 0;
+	int i;
+	printf("Zadejte cislo pro ciferny soucet: ");
+	scanf_s("%d", &cifra);
+	while (cifra > 0)
+	{
+		i = cifra % 10;
+		soucet = soucet + i;
+		cifra = cifra / 10;
+	}
+	printf("%d", soucet);
+	return 0;
+
+}
